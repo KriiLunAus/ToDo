622e60dd878c1d9ae347748c774922c073e384d9
KriiLunAus <146242487+KriiLunAus@users.noreply.github.com>
2024-09-17 08:19:18 +0300

    added locale storage

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+
 function addClick(){
     if(inputBox.value === ''){
        window.alert("Type something")
     }else{
         const li = document.createElement("li");
+        
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
 
@@ -14,13 +16,30 @@ function addClick(){
     }
 
     inputBox.value = '';
+
+    saveTasks()
 }
 
 
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+
+        saveTasks()
     }else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        
+        saveTasks()
     }
 }, false);
+
+
+
+function saveTasks(){
+    localStorage.setItem("tasks", listContainer.innerHTML);
+}
+
+function showTasks(){
+    listContainer.innerHTML = localStorage.getItem("tasks");
+}
+showTasks();
