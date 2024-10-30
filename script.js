1f90d58d40e737ee759dabc16d6da39dc575c33f
KriiLunAus <146242487+KriiLunAus@users.noreply.github.com>
2024-10-30 13:11:01 +0200

    Removed autocomplete

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-function addClick(){
-    if(inputBox.value === ''){
-       window.alert("Type something")
-    }else{
-        const li = document.createElement("li");
-        
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
+function addClick() {
+  if (inputBox.value === "") {
+    alert("Type something");
+  } else {
+    const li = document.createElement("li");
 
-        const span = document.createElement("span");
-        span.innerHTML =  "\u00d7"
-        li.appendChild(span);
-    }
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
 
-    inputBox.value = '';
+    const span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
 
-    saveTasks()
+  inputBox.value = "";
+
+  saveTasks();
 }
 
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
+      saveTasks();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
 
-        saveTasks()
-    }else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        
-        saveTasks()
+      saveTasks();
     }
-}, false);
+  },
+  false
+);
 
-
-
-function saveTasks(){
-    localStorage.setItem("tasks", listContainer.innerHTML);
+function saveTasks() {
+  localStorage.setItem("tasks", listContainer.innerHTML);
 }
 
-function showTasks(){
-    listContainer.innerHTML = localStorage.getItem("tasks");
+function showTasks() {
+  listContainer.innerHTML = localStorage.getItem("tasks");
 }
 showTasks();
